9d01e246221c75d02ab6def28d496d537d45db3b
Lisa Trevis <l_trevis@hotmail.com>
2021-03-10 10:59:36 -0800

    Add remove button

---
+++ components/ProductDisplay.js
@@ -0,0 +1,107 @@
+app.component('product-display', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template:
+    /*html*/
+    `<div class="product-display">
+        <div class="product-container">
+          <div class="product-image">
+            <img v-bind:src="image" />
+          </div>
+          <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+            <ul>
+              <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+
+            <div
+              v-for="(variant, index) in variants"
+              :key="variant.id"
+              @mouseover="updateVariant(index)"
+              class="color-circle"
+              :style="{ backgroundColor: variant.color }"
+            ></div>
+
+            <button
+              class="button"
+              :class="{ disabledButton: !inStock }"
+              :disabled="!inStock"
+              v-on:click="addToCart"
+            >
+              Add to Cart
+            </button>
+            <button
+              class="button"
+              :class="{ disabledButton: !inStock }"
+              :disabled="!inStock"
+              v-on:click="removeById"
+            >
+              Remove Item
+            </button>
+          </div>
+        </div>
+      </div>`,
+  data() {
+    return {
+      product: 'Elf Coats',
+      brand: 'Forest Fairy',
+      selectedVariant: 0,
+      details: ['Hand Made', 'Unique Design', 'Upcycled Materials'],
+      variants: [
+        {
+          id: 1,
+          color: 'hotpink',
+          image: './assets/images/candyfront.jpg',
+          quantity: 50,
+        },
+        {
+          id: 2,
+          color: 'teal',
+          image: './assets/images/oceanfront.jpg',
+          quantity: 0,
+        },
+        {
+          id: 3,
+          color: 'darkblue',
+          image: './assets/images/southwestfront.jpg',
+          quantity: 1,
+        },
+      ],
+    }
+  },
+  methods: {
+    addToCart() {
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+    },
+    removeById() {
+      this.$emit('remove-by-id', this.variants[this.selectedVariant].id)
+    },
+    updateVariant(index) {
+      this.selectedVariant = index
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    },
+    shipping() {
+      if (this.premium) {
+        return 'Free'
+      }
+      return '$12.99'
+    },
+  },
+})
--- main.js
+++ main.js
@@ -1,50 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 0,
-      product: 'Elf Coats',
-      brand: 'Forest Fairy',
-      selectedVariant: 0,
-      details: ['Hand Made', 'Unique Design', 'Upcycled Materials'],
-      variants: [
-        {
-          id: 1,
-          color: 'hotpink',
-          image: './assets/images/candyfront.jpg',
-          quantity: 50,
-        },
-        {
-          id: 2,
-          color: 'teal',
-          image: './assets/images/oceanfront.jpg',
-          quantity: 0,
-        },
-        {
-          id: 3,
-          color: 'darkblue',
-          image: './assets/images/southwestfront.jpg',
-          quantity: 1,
-        },
-      ],
+      cart: [],
+      premium: false,
     }
   },
   methods: {
-    addToCart() {
-      this.cart += 1
+    updateCart(id) {
+      this.cart.push(id)
     },
-    updateVariant(index) {
-      this.selectedVariant = index
-    },
-  },
-  computed: {
-    title() {
-      return this.brand + ' ' + this.product
-    },
-    image() {
-      return this.variants[this.selectedVariant].image
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].quantity
+    removeById(id) {
+      const index = this.cart.indexOf(id)
+      if (index > -1) {
+        this.cart.splice(index, 1)
+      }
     },
   },
 })
